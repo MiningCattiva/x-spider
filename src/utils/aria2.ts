6d3a2db18eb083fe34a81c7fe6c5b8aa1fb47ea5
MoyuScript <moyuscript@gmail.com>
2024-01-20 22:54:51 +0800

    feat: aria2 添加 notification

--- src/utils/aria2.ts
+++ src/utils/aria2.ts
@@ -14,12 +14,17 @@ class Aria2 {
   #invokeId = 0;
 
   onReady = new EventEmitter();
+  onDownloadStart = new EventEmitter<string>();
+  onDownloadPause = new EventEmitter<string>();
+  onDownloadStop = new EventEmitter<string>();
+  onDownloadComplete = new EventEmitter<string>();
+  onDownloadError = new EventEmitter<string>();
 
   constructor() {
-    this.#initiate();
+    this.#initialize();
   }
 
-  async #initiate() {
+  async #initialize() {
     this.#command = Command.sidecar('binaries/aria2c', [
       '--enable-rpc',
       '--rpc-secret',
@@ -47,6 +52,33 @@ class Aria2 {
     console.error(message);
   }
 
+  #onWsMessage(ev: MessageEvent) {
+    const data = JSON.parse(ev.data);
+    if (data.id) return;
+
+    const gid = () => data.params[0];
+
+    switch (data.method) {
+      case 'aria2.onDownloadStart':
+        this.onDownloadStart.emit(gid());
+        break;
+      case 'aria2.onDownloadPause':
+        this.onDownloadPause.emit(gid());
+        break;
+      case 'aria2.onDownloadStop':
+        this.onDownloadStop.emit(gid());
+        break;
+      case 'aria2.onDownloadComplete':
+        this.onDownloadComplete.emit(gid());
+        break;
+      case 'aria2.onDownloadError':
+        this.onDownloadError.emit(gid());
+        break;
+      default:
+        break;
+    }
+  }
+
   #connectWs() {
     this.#ws = new WebSocket(`ws://127.0.0.1:${this.#port}/jsonrpc`);
     this.#ws.addEventListener('open', () => {
@@ -56,6 +88,7 @@ class Aria2 {
     this.#ws.addEventListener('error', (ev) => {
       console.error('Aria websocket error', ev);
     });
+    this.#ws.addEventListener('message', this.#onWsMessage.bind(this));
   }
 
   #kill() {
