f56e006d5672d75c222f8dc970d44523e7c1166c
MiningCattiva <159104748+MiningCattiva@users.noreply.github.com>
2024-02-28 16:49:53 +0800

    fix: 默认配置不启用代理

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -2,13 +2,13 @@ import { Settings } from '../interfaces/Settings';
 
 export const DEFAULT_SETTINGS: Settings = {
   proxy: {
-    enable: true,
+    enable: false,
     url: 'http://127.0.0.1:7890',
   },
   download: {
     savePath: '',
     fileNameTemplate: '%POST_TIME% %USER_SCREEN_NAME% %MEDIA_ID%%EXT%',
-    sameFileSkip: false,
+    sameFileSkip: true,
   },
 };
 
