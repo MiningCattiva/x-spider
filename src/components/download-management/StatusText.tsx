3e08cda7d86add476a6cd0e10f2a0bf34faef866
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2024-04-16 21:13:46 +0800

    feat:  显示下载错误理由

--- src/components/download-management/StatusText.tsx
+++ src/components/download-management/StatusText.tsx
@@ -9,10 +9,13 @@ export interface StatusTextProps {
 }
 
 export const StatusText: React.FC<StatusTextProps> = ({ task }) => {
-  const [text, className] = R.cond([
+  const [text, className] = R.cond<[DownloadTask], string[]>([
     [R.propEq('waiting', 'status'), R.always(['🟡等待中', 'text-gray-500'])],
     [R.propEq('active', 'status'), R.always(['▶︎下载中', ''])],
-    [R.propEq('error', 'status'), R.always(['❌️下载错误', 'text-red-500'])],
+    [
+      R.propEq('error', 'status'),
+      (task) => [`❌️下载错误：${task.error || '未知原因'}`, 'text-red-500'],
+    ],
     [R.propEq('paused', 'status'), R.always(['⏸︎已暂停', 'text-yellow-500'])],
     [R.propEq('complete', 'status'), R.always(['✅︎已完成', 'text-green-500'])],
     [R.T, R.always(['未知状态'])],
