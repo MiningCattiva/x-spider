3b0ff69f61c62c26d3a80aea679fb3da99190782
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2024-04-16 20:22:24 +0800

    feat: 新增简单的网络请求重试机制

--- src/ipc/network.ts
+++ src/ipc/network.ts
@@ -3,6 +3,9 @@ import { Response } from '../interfaces/Response';
 import { RequestOptions } from '../interfaces/RequestOptions';
 import * as R from 'ramda';
 import { useSettingsStore } from '../stores/settings';
+import { delay } from '../utils';
+
+const MAX_RETRY_COUNT = 7;
 
 export async function request(options: RequestOptions) {
   const url = new URL(options.url);
@@ -14,15 +17,35 @@ export async function request(options: RequestOptions) {
   }
 
   const settings = useSettingsStore.getState();
-  return requestInternal(
-    R.defaultTo('GET', options.method),
-    url.href,
-    R.defaultTo('', options.body),
-    settings.proxy.enable,
-    settings.proxy.useSystem ? '' : settings.proxy.url,
-    R.defaultTo({}, options.headers),
-    options.responseType,
-  );
+  let remainingRetryCount = MAX_RETRY_COUNT;
+  let retryDelay = 100;
+  let lastErr: any;
+
+  while (remainingRetryCount > 0) {
+    try {
+      return await requestInternal(
+        R.defaultTo('GET', options.method),
+        url.href,
+        R.defaultTo('', options.body),
+        settings.proxy.enable,
+        settings.proxy.useSystem ? '' : settings.proxy.url,
+        R.defaultTo({}, options.headers),
+        options.responseType,
+      );
+    } catch (err: any) {
+      lastErr = err;
+      log.warn(
+        `Request failed, retry after ${retryDelay}ms, remaining retry count: ${remainingRetryCount}`,
+        err,
+      );
+      await delay(retryDelay);
+      remainingRetryCount--;
+      retryDelay *= 2;
+    }
+  }
+
+  log.error('Max retry count reached, last error:', lastErr);
+  throw lastErr;
 }
 
 async function requestInternal(
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
